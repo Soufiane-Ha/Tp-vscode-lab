9b5d6d157309aa91bc9e34a7eaed90e44e538186
Soufiane-Ha <soufianehammadi02@email.com>
2026-05-06 17:48:29 +0100

    feat: implement Calculator class and basic application logic in App class

---
+++ docs/gradle-lan/app/src/main/java/org/example/Calculator.java
@@ -0,0 +1,11 @@
+import org.apache.commons.lang3.StringUtils;
+
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+}
